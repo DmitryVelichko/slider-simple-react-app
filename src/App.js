09666a2fb9e6f1dabdfb184d1c0fb28f7c830f7b
Dmitry Velichko <42185328+DmitryVelichko@users.noreply.github.com>
2021-07-05 20:06:47 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
+
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = React.useState(0);
